577cd79a9845d1e3db5fe7c33a906d76b36625c8
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-02-26 21:51:00 +0100

    initial commit

---
+++ src/test/java/com/artemnizhnyk/testcontainers/TestContainersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artemnizhnyk.testcontainers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestContainersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
